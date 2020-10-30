0df9b4c40381525fae740ec377726f1d3b546ef8
A-091 <88.choice.r53@gmail.com>
2020-10-30 08:22:20 +0000

    four

--- resources/views/admin/create/inquiry.blade.php
+++ resources/views/admin/create/inquiry.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.admin')
 @section('title','お問合せフォーム')
-@include('layouts.header')
-@include('layouts.navbar')
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 mx-auto">
             <h2>お問合せフォーム</h2>
-            <p>お問合せフォームをご利用の際は、必ず下記「"<a href="">個人情報保護方針</a>"」を一読ください。
+            <p>お問合せフォームをご利用の際は、必ず下記「"<a href="#">個人情報保護方針</a>"」を一読ください。
             <br>内容に同意していただけましたら、下記フォームに必要事項をご入力の上、「入力確認画面へ」ボタンをクリックしてください。
             <br>なお、お問い合わせの内容によっては、ご返答が遅れる場合がございます。ご了承ください。
             </p>
@@ -20,25 +18,30 @@
                 </ul>
                 @endif
                 <div class="form-group row">
-                    <label class="col-md-2">お問合せ内容</label>
+                    <label class="col-md-2">お問合せ内容</label><br>
+                    <span class="must">必須</span><br>
                     <div class="col-md-10">
                         <textarea class="form-control" name="contents" rows="20">{{ old('contents') }}</textarea>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-md-2">お名前</label>
+                    <span class="must">必須</span>
+                    
                     <div class="col-md-10">
                         <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-md-2">電話番号</label>
+                    <span class="must">必須</span>
                     <div class="col-md-10">
                         <input type="tel" class="form-control" name="tel" value="{{ old('tel') }}">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-md-2">メールアドレス</label>
+                    <span class="must">必須</span>
                     <div class="col-md-10">
                         <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                     </div>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">{{-- edgeに対応する --}}
         <meta name="viewport" content="width=device-width, initial-scale=1">{{-- スマートフォンで見たときなどに文字や画像の大きさを調整 --}}
-        <!-- CSRF Token -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">        <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">{{-- 全リクエストヘッダにトークンを追加 --}}
         <title>@yield('title')</title>{{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
         <!-- Scripts -->
@@ -17,12 +17,47 @@
         <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div id="app">
-        </div>
-            {{-- ここまでナビゲーションバー --}}
-            <main class="py-4">
-                @yield('content') {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
-            </main>    
+        <div class="container">
+        　　<div class="row">
+            　　<div id="header left">
+                    <p class="header text">阪神間で口コミNo.1　骨盤矯正・交通事故施術で評判の整骨院</p>
+                　　<a class="navbar brand" href="#">091整骨院</a>
+                </div>
+            </div>
+            <div class="row">
+                <div id="header right">
+                    <a class="tel">TEL ; 06-1234-5678</a><br>
+                    <a class="time">TEL予約受付時間 9:00～19:00 <span class="hot">日祝 休診</span></a><br>
+                    <a class="email">E-mail ; abc@xyz.ne.jp</a><br>
+                    <a class="web">WEB予約<span class="reception">24時間受付</span><br>
+                    <a href="reservation.cleate"></a>
+                </div>
+            </div>
+            <nav class="navbar bg-info navbar-expand-md">
+                <div class="row">
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarNav4">
+                        <div class="navbar nav">
+                            <a class="nav-item nav-link active" href="#">TOP<span class="sr-only">(current)</span></a>
+                            <a href="#">施術メニュー</a>
+                            <a href="#">お客様の声</a>
+                            <a href="#">お問合せ</a>
+                        </div>    
+                        <div class="navbar nav navbar right">
+                            <a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                        </div>    
+                    </div>
+                </div>
+            </nav>
+            <div id="app">
+            </div>
+                <main class="py-4">
+                    @yield('content') {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
+                </main>
+            <footer class="footer">
+                <div class="container">
+                    <p id="copyright">(C) Copyright 091整骨院.</p>
+                </div>
+            </footer>
         </div>
     </body>
 </html>
